98bf1101cef16ccfe42476b1012e48ea293b662c
Nur Hossain <nurhossain.bpbl@outlook.com>
2022-02-20 17:16:35 +0600

    initial v2

--- m365linse.ps1
+++ m365linse.ps1
@@ -3,14 +3,40 @@ $usr=$env:username
 $grp=$env:License
 #$usr="nur1"
 #$grp="Business Basic"
+$grp1= (Get-ADGroup "Business Standard" ).DistinguishedName
+$grp2= (Get-ADGroup "Business Basic" ).DistinguishedName
+$grp3= (Get-ADGroup "EMS" ).DistinguishedName
+
 
 if ( (Get-ADUser $usr -Properties memberof).memberof -ccontains (Get-ADGroup $grp).DistinguishedName)
 {
-Write-Host $usr "Already exists in group" $grp
+   Write-Host $usr "Already exists in group" $grp
 
-}
-else
-{
-Add-ADGroupMember $grp -Members $usr
-Write-Host  $usr "has been successfully added to" $grp
-}
+   }
+    else
+        {
+         if ( (Get-ADUser $usr -Properties memberof).memberof -ccontains (Get-ADGroup $grp1).DistinguishedName)
+          {
+           Write-Host $usr "Already exists in group" $grp1
+           }
+           else
+                {
+                if ( (Get-ADUser $usr -Properties memberof).memberof -ccontains (Get-ADGroup $grp2).DistinguishedName)
+                    {
+                       Write-Host $usr "Already exists in group" $grp2
+                     }
+                     else
+                     {
+                     if ( (Get-ADUser $usr -Properties memberof).memberof -ccontains (Get-ADGroup $grp3).DistinguishedName)
+                         {
+                            Write-Host $usr "Already exists in group" $grp3
+
+                     }
+                     else {
+                     Add-ADGroupMember $grp -Members $usr
+                     Write-Host  $usr "has been successfully added to" $grp
+                     }
+                }
+            }
+     }
+ }

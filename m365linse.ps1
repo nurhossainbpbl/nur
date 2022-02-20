74cd1ff1a77ecb40de4f8da625950118c1fc6298
Nur Hossain <nurhossain.bpbl@outlook.com>
2022-02-20 15:01:17 +0600

    add condition

--- m365linse.ps1
+++ m365linse.ps1
@@ -1,10 +1,16 @@
 Import-Module -Name ActiveDirectory
-$username=$env:username
-$grpname=$env:License
+$usr=$env:username
+$grp=$env:License
+#$usr="nur1"
+#$grp="Business Basic"
 
+if ( (Get-ADUser $usr -Properties memberof).memberof -ccontains (Get-ADGroup $grp).DistinguishedName)
+{
+Write-Host $usr "Already exists in group" $grp
 
-if (Add-ADGroupMember  $grpname -Members $username){
-
-Write-Host 'User added successfully'
-
+}
+else
+{
+Add-ADGroupMember $grp -Members $usr
+Write-Host  $usr "has been successfully added to" $grp
 }
